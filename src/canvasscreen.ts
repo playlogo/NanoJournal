ed388cff2a404bb238a7925fec15348aea3b8197
playlogo <playlogo@yahoo.com>
2024-10-22 19:32:46 +0200

    Added a cursor

--- src/canvasscreen.ts
+++ src/canvasscreen.ts
@@ -1,14 +1,39 @@
 import { BG, FG } from "./colors.js";
 
-enum ScreenGlobalState {
-	WRITING = 0,
-	EXITING = 1,
-	HELP = 2,
-	SAVING = 3,
+const equals = (one: [number, number], two: [number, number]) => {
+	return one.every((val, index) => val === two[index]);
+};
+
+abstract class ScreenMode {
+	//abstract handleCommand(command: string): void;
 }
 
+class ScreenModeWriting extends ScreenMode {
+	/*
+	handleCommand(command: string) {
+		switch (command) {
+			case "CTL-g":
+				this.loadHelp();
+				break;
+			case "CTL-x":
+				this.exit();
+				break;
+			case "CTL-r":
+				this.reload();
+				break;
+		}
+	}*/
+}
+
+const ScreenGlobalState = {
+	WRITING: new ScreenModeWriting(),
+	EXITING: 1,
+	HELP: 2,
+	SAVING: 3,
+};
+
 class ScreenState {
-	mode: ScreenGlobalState = ScreenGlobalState.WRITING;
+	mode: any = ScreenGlobalState.WRITING;
 
 	fileName = "";
 	content = [
@@ -22,10 +47,68 @@ class ScreenState {
 	status = "";
 	statusStyle: "full" | "middle" = "middle";
 
+	cursorPosition = { x: 0, y: 0 };
+
 	updateStatus() {
 		this.status = `[ ${this.fileName.length == 0 ? "New Buffer" : this.fileName} | ${
 			this.content.length
 		} lines ]`;
+	}
+
+	moveCursor(direction: [number, number]) {
+		// Move to left
+		if (equals(direction, [-1, 0])) {
+			// Start of line
+			if (this.cursorPosition.x === 0) {
+				if (this.cursorPosition.y === 0) {
+					// Can't go any further
+				} else {
+					// Move cursor to end of prev line
+					this.cursorPosition.y -= 1;
+					this.cursorPosition.x = this.content[this.cursorPosition.y].length + 1;
+				}
+			} else {
+				this.cursorPosition.x -= 1;
+			}
+
+			return;
+		}
+
+		// Move to the right
+		if (equals(direction, [1, 0])) {
+			// End of line
+			if (this.cursorPosition.x === this.content[this.cursorPosition.y].length + 1) {
+				if (this.cursorPosition.y === this.content.length + 1) {
+					// Can't go further
+				} else {
+					// Move cursor to start of next line
+					this.cursorPosition.x = 0;
+					this.cursorPosition.y += 1;
+				}
+			} else {
+				this.cursorPosition.x += 1;
+			}
+			return;
+		}
+
+		//TODO: Keep x position on vertical move
+		// Move down
+		if (equals(direction, [0, -1])) {
+			if (this.cursorPosition.y === this.content.length + 1) {
+				// At the bottom...
+			} else {
+				this.cursorPosition.y += 1;
+			}
+		}
+
+		// Move up
+		if (equals(direction, [0, 1])) {
+			if (this.cursorPosition.y === 0) {
+				// At the top...
+			} else {
+				this.cursorPosition.y -= 1;
+			}
+		}
 	}
 }
 
@@ -65,8 +148,38 @@ export class CanvasScreen {
 		// Draw text
 		this._render_buffer();
 
+		// Draw cursor
+		this._render_cursor();
+
 		// Render menu at bottom
 		this._render_bottom_menu();
+	}
+
+	_render_cursor() {
+		const pos = [
+			this.style.padding + this.state.cursorPosition.x * this.context.measureText(" ").width,
+			this.style.padding + 22 + this.state.cursorPosition.y * 18,
+		];
+
+		if (document.hasFocus() || true) {
+			this.context.fillStyle = FG;
+			this.context.fillRect(pos[0], pos[1], 10, 18);
+
+			// Redraw character
+			if (this.state.cursorPosition.y <= this.state.content.length) {
+				if (this.state.cursorPosition.x <= this.state.content[this.state.cursorPosition.y].length) {
+					this.context.fillStyle = BG;
+					this.context.fillText(
+						this.state.content[this.state.cursorPosition.y][this.state.cursorPosition.x],
+						pos[0],
+						pos[1] + 14
+					);
+				}
+			}
+		} else {
+			this.context.strokeStyle = FG;
+			this.context.strokeRect(pos[0], pos[1], 10, 18);
+		}
 	}
 
 	_render_buffer() {
@@ -97,7 +210,7 @@ export class CanvasScreen {
 	}
 
 	_render_bottom_menu() {
-		let options = [];
+		let options: string[] = [];
 
 		switch (this.state.mode) {
 			case ScreenGlobalState.WRITING:
@@ -189,7 +302,23 @@ export class CanvasScreen {
 	}
 
 	// Handle keyboard input
-	key(text: KeyboardEvent) {
-		console.log(text);
+	key(event: KeyboardEvent) {
+		// Shortcuts
+		if (event.ctrlKey) {
+			//this.state.handleCommand("CTL-" + event.key);
+		}
+		if (["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight"].includes(event.key)) {
+			this.state.moveCursor(
+				event.key === "ArrowLeft"
+					? [-1, 0]
+					: event.key === "ArrowRight"
+					? [1, 0]
+					: event.key === "ArrowDown"
+					? [0, -1]
+					: event.key === "ArrowUp"
+					? [0, 1]
+					: [0, 0]
+			);
+		}
 	}
 }
